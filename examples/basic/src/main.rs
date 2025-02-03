d8ade8ae8c2ead5512e260abf2db5cba833cbfbe
cyypherus <ejjonny@gmail.com>
2025-02-03 16:53:01 -0700

    render & layout text properly

--- examples/basic/src/main.rs
+++ examples/basic/src/main.rs
@@ -7,26 +7,32 @@ struct AppState {
 
 fn main() {
     App::start(
-        AppState::default(),
+        AppState {
+            text: "The scale factor is calculated differently on different platforms:".to_string(),
+        },
         dynamic_node(|s: &mut AppState| {
-            stack(vec![text(id!(), s.text.clone())
-                .fill(Color::WHITE)
-                .align(TextAlign::Leading)
-                .view()
-                .on_key(|s: &mut AppState, key| match key {
-                    Key::Named(NamedKey::Space) => s.text.push(' '),
-                    Key::Named(NamedKey::Enter) => s.text.push('\n'),
-                    Key::Named(NamedKey::Backspace) => {
-                        s.text.pop();
-                    }
-                    Key::Character(c) => s.text.extend(c.as_str().chars()),
-                    Key::Named(_) => (),
-                })
-                .finish()
-                .width(300.)
-                .attach_under(
+            row(vec![
+                space(),
+                column(vec![
                     rect(id!()).stroke(Color::WHITE, 1.).view().finish(),
-                )])
+                    text(id!(), s.text.clone())
+                        .fill(Color::WHITE)
+                        .align(TextAlign::Leading)
+                        .view()
+                        .on_key(|s: &mut AppState, key| match key {
+                            Key::Named(NamedKey::Space) => s.text.push(' '),
+                            Key::Named(NamedKey::Enter) => s.text.push('\n'),
+                            Key::Named(NamedKey::Backspace) => {
+                                s.text.pop();
+                            }
+                            Key::Character(c) => s.text.extend(c.as_str().chars()),
+                            Key::Named(_) => (),
+                        })
+                        .finish(),
+                    rect(id!()).stroke(Color::WHITE, 1.).view().finish(),
+                ]),
+                space(),
+            ])
         }),
     )
 }
@@ -43,60 +49,60 @@ fn main() {
 //     button(id!(i as u64)),
 // ),
 
-#[derive(Debug, Clone)]
-struct ButtonState<F> {
-    hovered: bool,
-    depressed: bool,
-    clicked: bool,
-    state: F,
-    action: fn(&mut F),
-}
+// #[derive(Debug, Clone)]
+// struct ButtonState<F> {
+//     hovered: bool,
+//     depressed: bool,
+//     clicked: bool,
+//     state: F,
+//     action: fn(&mut F),
+// }
 
-impl<F> ButtonState<F> {
-    fn new(state: F, action: fn(&mut F)) -> Self {
-        Self {
-            hovered: false,
-            depressed: false,
-            clicked: false,
-            state,
-            action,
-        }
-    }
-}
+// impl<F> ButtonState<F> {
+//     fn new(state: F, action: fn(&mut F)) -> Self {
+//         Self {
+//             hovered: false,
+//             depressed: false,
+//             clicked: false,
+//             state,
+//             action,
+//         }
+//     }
+// }
 
-fn button<'n, F: 'n>(id: u64) -> Node<'n, RcUi<ButtonState<F>>> {
-    dynamic_node(move |ui: &mut ButtonState<F>| {
-        stack(vec![rect(id!(id))
-            .stroke(
-                {
-                    match (ui.depressed, ui.hovered) {
-                        (_, true) => Color::from_rgb8(90, 90, 90),
-                        (true, false) => Color::from_rgb8(80, 30, 30),
-                        (false, false) => Color::from_rgb8(10, 10, 10),
-                    }
-                },
-                1.,
-            )
-            .fill(match (ui.depressed, ui.hovered) {
-                (true, true) => Color::from_rgb8(60, 5, 5),
-                (false, true) => Color::from_rgb8(90, 20, 20),
-                (true, false) => Color::from_rgb8(30, 30, 30),
-                (false, false) => Color::from_rgb8(10, 10, 10),
-            })
-            .corner_rounding(if ui.hovered { 10. } else { 5. })
-            .view()
-            .on_hover(|state: &mut ButtonState<F>, hovered| state.hovered = hovered)
-            .on_click(
-                move |state: &mut ButtonState<F>, click_state| match click_state {
-                    ClickState::Started => state.depressed = true,
-                    ClickState::Cancelled => state.depressed = false,
-                    ClickState::Completed => {
-                        state.clicked = true;
-                        (state.action)(&mut state.state);
-                        state.depressed = false;
-                    }
-                },
-            )
-            .finish()])
-    })
-}
+// fn button<'n, F: 'n>(id: u64) -> Node<'n, RcUi<ButtonState<F>>> {
+//     dynamic_node(move |ui: &mut ButtonState<F>| {
+//         stack(vec![rect(id!(id))
+//             .stroke(
+//                 {
+//                     match (ui.depressed, ui.hovered) {
+//                         (_, true) => Color::from_rgb8(90, 90, 90),
+//                         (true, false) => Color::from_rgb8(80, 30, 30),
+//                         (false, false) => Color::from_rgb8(10, 10, 10),
+//                     }
+//                 },
+//                 1.,
+//             )
+//             .fill(match (ui.depressed, ui.hovered) {
+//                 (true, true) => Color::from_rgb8(60, 5, 5),
+//                 (false, true) => Color::from_rgb8(90, 20, 20),
+//                 (true, false) => Color::from_rgb8(30, 30, 30),
+//                 (false, false) => Color::from_rgb8(10, 10, 10),
+//             })
+//             .corner_rounding(if ui.hovered { 10. } else { 5. })
+//             .view()
+//             .on_hover(|state: &mut ButtonState<F>, hovered| state.hovered = hovered)
+//             .on_click(
+//                 move |state: &mut ButtonState<F>, click_state| match click_state {
+//                     ClickState::Started => state.depressed = true,
+//                     ClickState::Cancelled => state.depressed = false,
+//                     ClickState::Completed => {
+//                         state.clicked = true;
+//                         (state.action)(&mut state.state);
+//                         state.depressed = false;
+//                     }
+//                 },
+//             )
+//             .finish()])
+//     })
+// }
